0b406f6928137c3240d42d25045f75e30b0cd1d2
jiayi-ren <jiayiren95@gmail.com>
2020-10-03 16:20:29 -0700

    remove temp console.log

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,8 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const Navigation = () => {
 
-    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-    useEffect(()=>{console.log(isAuthenticated)},[isAuthenticated])
+    const { loginWithRedirect, logout } = useAuth0();
 
     return(
         <div className="nav">
