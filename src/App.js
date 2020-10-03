57298a331692035be19b4613e3a056385ad9b46e
jiayi-ren <jiayiren95@gmail.com>
2020-10-03 15:47:20 -0700

    add auth0 provider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import { Route, Redirect, useHistory} from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
 import InvoicesDash from './components/Invoices/InvoicesDash';
@@ -11,52 +11,65 @@ import ClientsGen from './components/Clients/ClientsGen';
 import Settings from './components/Settings/Settings';
 import ItemsDash from './components/Items/ItemsDash';
 import ItemsGen from './components/Items/ItemsGen';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { auth0Config } from './utils/auth0Config';
+
 
 function App() {
+
+  const history = useHistory();
+
+  const onRedirectCallback = (appState) => {
+    history.push(appState?.returnTo || window.location.pathname);
+  };
+
   return (
-    <Router>
-      <Navigation />
+      <Auth0Provider
+        {...auth0Config}
+        onRedirectCallback={onRedirectCallback}
+      >
+        <Navigation />
 
-      {/* Invoices Routes*/}
-      <Route exact path="/">
-        <Redirect to="/invoices" />
-      </Route>
-      <Route exact path="/invoices">
-        <InvoicesDash />
-      </Route>
-      <Route exact path="/invoices/new">
-        <InvoicesGen />
-      </Route>
+        {/* Invoices Routes*/}
+        <Route exact path="/">
+          <Redirect to="/invoices" />
+        </Route>
+        <Route exact path="/invoices">
+          <InvoicesDash />
+        </Route>
+        <Route exact path="/invoices/new">
+          <InvoicesGen />
+        </Route>
 
-      {/* Estimates Routes */}
-      <Route exact path="/estimates">
-        <EstimatesDash />
-      </Route>
-      <Route exact path="/estimates/new">
-        <EstimatesGen />
-      </Route>
+        {/* Estimates Routes */}
+        <Route exact path="/estimates">
+          <EstimatesDash />
+        </Route>
+        <Route exact path="/estimates/new">
+          <EstimatesGen />
+        </Route>
 
-      {/* Clients Routes */}
-      <Route exact path="/clients">
-        <ClientsDash />
-      </Route>
-      <Route exact path="/clients/new">
-        <ClientsGen />
-      </Route>
+        {/* Clients Routes */}
+        <Route exact path="/clients">
+          <ClientsDash />
+        </Route>
+        <Route exact path="/clients/new">
+          <ClientsGen />
+        </Route>
 
-      {/* Items Routes */}
-      <Route exact path="/items">
-        <ItemsDash />
-      </Route>
-      <Route exact path="/items/new">
-        <ItemsGen />
-      </Route>
+        {/* Items Routes */}
+        <Route exact path="/items">
+          <ItemsDash />
+        </Route>
+        <Route exact path="/items/new">
+          <ItemsGen />
+        </Route>
 
-      {/* Settings Routes */}
-      <Route exact path="/settings">
-        <Settings />
-      </Route>
-    </Router>
+        {/* Settings Routes */}
+        <Route exact path="/settings">
+          <Settings />
+        </Route>
+      </Auth0Provider>
   );
 }
 
