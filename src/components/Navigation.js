4db6b230688f0e9059f0ecedd32477f4e72de5df
jiayi-ren <jiayiren95@gmail.com>
2020-10-04 16:49:40 -0700

    add basic styles for navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,24 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useAuth0 } from "@auth0/auth0-react";
+import { AppBar, Button, makeStyles, MenuItem, Select, Toolbar } from '@material-ui/core';
+import TranslateIcon from '@material-ui/icons/Translate';
+
+const useStyles = makeStyles({
+    nav:{
+        backgroundColor: 'white',
+        color: 'black',
+        textDecoration: 'none'
+    },
+    navLink: {
+        textDecoration: 'none',
+        color: 'black'
+    },
+    selected: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        padding: '0 30px',
+    },
+    language: {
+        textDecoration: "none",
+    }
+})
 
 const Navigation = () => {
 
     const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+    const [language, setLanguage] = useState("english")
+    const classes = useStyles();
 
+    const languageChange = (event) => {
+        setLanguage(event.target.value);
+      };
+    
     return(
-        <div className="nav">
-            <h2>EzInvoice</h2>
-            <div>
-                <NavLink to="/invoices" activeClassName="selected">Invoices</NavLink>
-                <NavLink to="/estimates" activeClassName="selected">Estimates</NavLink>
-                <NavLink to="/clients" activeClassName="selected">Clients</NavLink>
-                <NavLink to="/items" activeClassName="selected">Items</NavLink>
-                <NavLink to="/settings" activeClassName="selected">Settings</NavLink>
-                {!isAuthenticated && <NavLink to="/login" onClick={() => {loginWithRedirect()}}>Login</NavLink>}
-                {isAuthenticated && <NavLink to="/" onClick={() => {logout()}}>Logout</NavLink>}
+        <div >
+            <AppBar position="static" className={classes.nav}>
+                <Toolbar>
+                    <h2>ezInvoice</h2>
+                    <Button>
+                        <NavLink to="/invoices" activeClassName={classes.selected} className={classes.navLink}>Invoices</NavLink>
+                    </Button>
+                    <Button>
+                        <NavLink to="/estimates" activeClassName={classes.selected} className={classes.navLink}>Estimates</NavLink></Button>
+                    <Button>
+                        <NavLink to="/clients" activeClassName={classes.selected} className={classes.navLink}>Clients</NavLink>
+                    </Button>
+                    <Button>
+                        <NavLink to="/items" activeClassName={classes.selected} className={classes.navLink}>Items</NavLink>
+                    </Button>
+                    <Button>
+                        <NavLink to="/settings" activeClassName={classes.selected} className={classes.navLink}>Settings</NavLink>
+                    </Button>
+                    <Button>
+                        <TranslateIcon></TranslateIcon>
+                        <Select
+                            value={language}
+                            onChange={languageChange}
+                            disableUnderline={true}
+                        >
+                            <MenuItem value="english">English</MenuItem>
+                            <MenuItem value="chinese">中文</MenuItem>
+                        </Select>
+                    </Button>
+                    <Button>
+                        {!isAuthenticated && <NavLink to="/login" onClick={() => {loginWithRedirect()}} className={classes.navLink}>Login</NavLink>}
+                        {isAuthenticated && <NavLink to="/" onClick={() => {logout()}} className={classes.navLink}>Logout</NavLink>}
+                    </Button>
+                    
+                </Toolbar>
+                
                 <span id="lang-icon"></span>
-            </div>
+            </AppBar>
         </div>
     )
 }
