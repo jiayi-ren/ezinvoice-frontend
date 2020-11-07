6556579d3d1afee3088693ac31bca14804713159
jiayi-ren <jiayiren95@gmail.com>
2020-11-06 22:32:57 -0800

    integrate clients state into redux store

--- src/state/store.js
+++ src/state/store.js
@@ -2,9 +2,11 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { combineReducers } from 'redux';
 import { userReducer as user } from './user/userReducer';
+import { clientReducer as clients } from './clients/clientReducer';
 
 const rootReducer = combineReducers({
     user,
+    clients,
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
