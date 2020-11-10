dc617cd2e4dbbd55f6106de510d8fe361f1d6833
jiayi-ren <jiayiren95@gmail.com>
2020-11-09 21:51:26 -0800

    integrate items into redux store

--- src/state/store.js
+++ src/state/store.js
@@ -3,10 +3,12 @@ import thunk from 'redux-thunk';
 import { combineReducers } from 'redux';
 import { userReducer as user } from './user/userReducer';
 import { clientReducer as clients } from './clients/clientReducer';
+import { itemReducer as items } from './items/itemReducer';
 
 const rootReducer = combineReducers({
     user,
     clients,
+    items,
 });
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
