8c1e7008c7ec59d5e9774340ad9075631db5940a
Jiayi Ren <45396888+jiayi-ren@users.noreply.github.com>
2020-12-29 22:39:40 -0800

    fix saveAlert import

--- src/components/Clients/ClientsGen.js
+++ src/components/Clients/ClientsGen.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import ClientsTemplate from './ClientsTemplate';
 import { useHistory, useParams } from 'react-router-dom';
 import { Button } from '@material-ui/core';
-import SaveAlert from '../Common/SaveAlert';
+import { SaveAlert } from '../Common/Alerts';
 import { convertKeysCase } from '../../utils/caseConversion';
 import {
     createClientAct,
