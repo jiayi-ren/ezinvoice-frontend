dd53f7f6f932164593eb007f1994d058ec278f3f
Jiayi Ren <45396888+jiayi-ren@users.noreply.github.com>
2020-12-29 22:36:16 -0800

    add deleteAlerts

---
+++ src/components/Common/Alerts.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import { Button, Collapse, CircularProgress } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+
+export const SaveAlert = props => {
+    const {
+        isAuthenticated,
+        history,
+        saveAlertOpen,
+        setSaveAlertOpen,
+        isSaved,
+        path,
+        status,
+    } = props;
+
+    return (
+        <Collapse in={saveAlertOpen}>
+            {status === 'loading' && (
+                <Alert
+                    severity="info"
+                    icon={
+                        <CircularProgress
+                            style={{
+                                width: '20px',
+                                height: '20px',
+                            }}
+                        />
+                    }
+                >
+                    Saving
+                </Alert>
+            )}
+            {isSaved &&
+                ((isAuthenticated && status === 'succeeded') ||
+                    !isAuthenticated) && (
+                    <Alert severity="success">Saved Successfully</Alert>
+                )}
+            {isAuthenticated && status === 'failed' && (
+                <Alert severity="error">Failed to save</Alert>
+            )}
+            {!isSaved && (
+                <Alert
+                    severity="warning"
+                    action={
+                        <>
+                            <Button
+                                aria-label="close"
+                                color="inherit"
+                                size="small"
+                                onClick={() => {
+                                    history.push(`${path}`);
+                                }}
+                            >
+                                Yes
+                            </Button>
+                            <Button
+                                aria-label="close"
+                                color="inherit"
+                                size="small"
+                                onClick={() => {
+                                    setSaveAlertOpen(false);
+                                }}
+                            >
+                                No
+                            </Button>
+                        </>
+                    }
+                >
+                    Your work has not been saved. Do you still want to leave?
+                </Alert>
+            )}
+        </Collapse>
+    );
+};
+
+export const DeleteAlert = props => {
+    const {
+        deleteAlertOpen,
+        setDeleteAlertOpen,
+        isDeleted,
+        deleteAction,
+        status,
+    } = props;
+
+    return (
+        <Collapse in={deleteAlertOpen}>
+            {status === 'loading' && (
+                <Alert
+                    severity="info"
+                    icon={
+                        <CircularProgress
+                            style={{
+                                width: '20px',
+                                height: '20px',
+                            }}
+                        />
+                    }
+                >
+                    Deleting
+                </Alert>
+            )}
+            {isDeleted && status === 'succeeded' && (
+                <Alert severity="success">Deleted Successfully</Alert>
+            )}
+            {status === 'failed' && (
+                <Alert severity="error">Failed to delete</Alert>
+            )}
+            {!isDeleted && (
+                <Alert
+                    severity="warning"
+                    action={
+                        <>
+                            <Button
+                                aria-label="close"
+                                color="inherit"
+                                size="small"
+                                onClick={deleteAction}
+                            >
+                                Yes
+                            </Button>
+                            <Button
+                                aria-label="close"
+                                color="inherit"
+                                size="small"
+                                onClick={() => {
+                                    setDeleteAlertOpen(false);
+                                }}
+                            >
+                                No
+                            </Button>
+                        </>
+                    }
+                >
+                    Confirm you want to delete (This action cannot be undone)
+                </Alert>
+            )}
+        </Collapse>
+    );
+};
