d4a35067c39fc6ae087137579beb5b5a8b6ca19d
Jiayi Ren <45396888+jiayi-ren@users.noreply.github.com>
2020-12-29 22:50:33 -0800

    lift states up, add 'edit' headCell

--- src/components/Invoices/InvoicesList.js
+++ src/components/Invoices/InvoicesList.js
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Table from '../Common/Table/Table';
-import { useAuth0 } from '@auth0/auth0-react';
 import { FormControlLabel, Switch } from '@material-ui/core';
 
 const headCells = [
@@ -9,28 +8,12 @@ const headCells = [
     { id: 'address', numeric: false, disablePadding: false, label: 'Address' },
     { id: 'date', numeric: false, disablePadding: false, label: 'Date' },
     { id: 'amount', numeric: true, disablePadding: false, label: 'Amount' },
+    { id: 'edit', numeric: true, disablePadding: false, label: '' },
 ];
 
-const InvoicesList = () => {
-    const { isAuthenticated } = useAuth0;
-    const [invoices, setInvoices] = useState([]);
+const InvoicesList = props => {
+    const { invoicesList, selected, setSelected } = props;
     const [dense, setDense] = useState(false);
-
-    useEffect(() => {
-        if (!isAuthenticated) {
-            const localInvoices = JSON.parse(
-                window.localStorage.getItem('invoices'),
-            );
-            setInvoices(localInvoices);
-        }
-    }, [isAuthenticated]);
-
-    useEffect(() => {
-        const localInvoices = JSON.parse(
-            window.localStorage.getItem('invoices'),
-        );
-        setInvoices(localInvoices);
-    }, []);
 
     const handleChangeDense = event => {
         setDense(event.target.checked);
@@ -44,11 +27,13 @@ const InvoicesList = () => {
                 }
                 label="Dense padding"
             />
-            <Table 
-                data={invoices}
-                dataType="Invoice"
+            <Table
+                data={invoicesList}
+                dataType="invoices"
                 dense={dense}
                 headCells={headCells}
+                selected={selected}
+                setSelected={setSelected}
             />
         </>
     );
